7f0b7370eeefd69ccd35f46ee0e72acecb4f9762
Takahiro Era <takahiro7114@gmail.com>
2019-11-16 12:00:31 +0900

    ttyの記述が抜けていたのを修正

--- generate_music.sh
+++ generate_music.sh
@@ -4,5 +4,5 @@ if [ $# -ne 4 ]; then
   echo "引数は4個必要です" 1>&2
   exit 1
 fi
-docker run -it --rm magenta python /2019gakusai/generate_midi.py $1 $2 $3 $4
-docker run -it --rm timidity midi-convert.sh $4
+docker-compose exec magenta python /2019gakusai/generate_midi.py $1 $2 $3 $4
+docker-compose exec timidity bash /midi-convert.sh $4
