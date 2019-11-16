f15af86d95d2aea63f05f624bccbc0b91b2634f7
Takahiro Era <takahiro7114@gmail.com>
2019-11-16 10:49:24 +0900

    commited at 10:49

--- generate_music.sh
+++ generate_music.sh
@@ -4,4 +4,5 @@ if [ $# -ne 4 ]; then
   echo "引数は4個必要です" 1>&2
   exit 1
 fi
-docker run --rm magenta python /2019gakusai/generate_midi.py $1 $2 $3 $4
+docker-compose run -it --rm magenta python /2019gakusai/generate_midi.py $1 $2 $3 $4
+docker-compose run -it --rm 
