1dbf5bbd0be1e1293abdcbd4ebfa497221e64704
Takahiro Era <takahiro7114@gmail.com>
2019-11-16 10:55:01 +0900

    commit 10:54

--- generate_music.sh
+++ generate_music.sh
@@ -4,5 +4,5 @@ if [ $# -ne 4 ]; then
   echo "引数は4個必要です" 1>&2
   exit 1
 fi
-docker-compose run -it --rm magenta python /2019gakusai/generate_midi.py $1 $2 $3 $4
-docker-compose run -it --rm 
+docker run -it --rm magenta python /2019gakusai/generate_midi.py $1 $2 $3 $4
+docker run -it --rm timidity midi-convert.sh $4
